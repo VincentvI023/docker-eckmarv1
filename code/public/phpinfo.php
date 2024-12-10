018e95575996b5305e9dee91e4651d87a5b0f43c
Vincent van Ingen <info@vincentvaningen.nl>
2024-12-10 15:56:39 +0100

    first commit

---
+++ code/public/phpinfo.php
@@ -0,0 +1,23 @@
+<?php
+
+// $testGD = get_extension_funcs("gd"); // Grab function list 
+// if (!$testGD){ echo "GD not even installed."; exit; }
+// echo"<pre>".print_r($testGD,true)."</pre>";
+
+echo phpinfo();
+
+// DB_CONNECTION=mysql
+// DB_HOST=docker-eckmarv1-db-1
+// DB_PORT=3306
+// DB_DATABASE=marketplace
+// DB_USERNAME=admin 
+// DB_PASSWORD=Admin123!#
+
+
+// CREATE USER 'superadmin'@'%' IDENTIFIED BY "admin34578";
+
+// GRANT ALL PRIVILEGES ON *.* TO 'superadmin'@'%';
+
+
+
+?>
