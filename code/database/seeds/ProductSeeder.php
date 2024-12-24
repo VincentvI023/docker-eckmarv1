a38f93a944968c51a0c46a5aeebe99575b87e553
Vincent van Ingen <info@vincentvaningen.nl>
2024-12-24 16:46:45 +0100

    Change ProductSeeder to run from JSON file

--- code/database/seeds/ProductSeeder.php
+++ code/database/seeds/ProductSeeder.php
@@ -6,186 +6,113 @@ use App\Product;
 
 class ProductSeeder extends Seeder
 {
+    private $jsonFilePath = 'products.json';
 
-    private $imagesArray = [];
-    private $fakerFactory;
-
-    /**
-     * How many products to create
-     * @var int
-     */
-    private $productsToCreate = 100;
-    /**
-     * How many images to create before reusing them
-     * @var int
-     */
-    private $maximumAmoutOfImagesToCreate = 5;
-
-    public function __construct() {
-        $this->fakerFactory = Faker\Factory::create();
-    }
-
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         $start = microtime(true);
-        $faker = $this->fakerFactory;
-        /*
-         * Categories
-         * Drinks
-         *      Alcoholic
-         *          Beer
-         *          Vine
-         *      Non Alcoholic
-         *          Sodas
-         *          Juices
-         */
-        $this->command->info('Creating categories...');
-        $drinks = new Category();
-        $drinks -> name = 'Drinks';
-        $drinks -> save();
+        
+        $this->command->info('Reading JSON file...');
+        $productsData = $this->readJsonFile();
 
-        // first level categories
-        $nonAlchoholic = new Category;
-        $nonAlchoholic -> name = 'Non-Alcoholic';
-        $nonAlchoholic -> parent_id = $drinks -> id;
-        $nonAlchoholic -> save();
-
-        $alcoholic = new Category;
-        $alcoholic -> name = 'Alcoholic';
-        $alcoholic -> parent_id = $drinks -> id;
-        $alcoholic -> save();
-
-        // second level categories
-        $beer = new Category;
-        $beer -> name = 'Beer';
-        $beer -> parent_id = $alcoholic -> id;
-        $beer -> save();
-
-        $vine = new Category;
-        $vine -> name = 'Vine';
-        $vine -> parent_id = $alcoholic -> id;
-        $vine -> save();
-
-        $sodas = new Category;
-        $sodas -> name = 'Sodas';
-        $sodas -> parent_id = $nonAlchoholic -> id;
-        $sodas -> save();
-
-        $juices = new Category;
-        $juices -> name = 'Juices';
-        $juices -> parent_id = $nonAlchoholic -> id;
-        $juices -> save();
-
-
-        /**
-         * Products
-         */
-
-        $this->command->info('Creating products...');
-        for($i = 0; $i <= $this->productsToCreate; $i++){
-            $newProduct = new Product;
-            $newProduct -> name = $faker->streetSuffix;
-            $newProduct -> description = $faker->sentence($nbWords = 6, $variableNbWords = true);
-            $newProduct -> rules = $faker->sentence($nbWords = 6, $variableNbWords = true);
-            $newProduct -> quantity = rand(1,20);
-            $newProduct -> mesure = 'item';
-            $newProduct -> coins = 'btc,xmr,stb';
-            $newProduct -> category_id = Category::inRandomOrder() -> first() -> id;
-            $newProduct -> user_id = \App\Vendor::inRandomOrder() -> first() -> id;
-            if($newProduct -> user_id == null) dd(\App\Vendor::all() -> first() -> id);
-            $newProduct -> save();
-
-            // Shipings
-            $newShiping = new \App\Shipping;
-            $newShiping -> product_id = $newProduct -> id;
-            $newShiping -> name = 'Shipping for ' . $newProduct -> name;
-            $newShiping -> price = mt_rand() / mt_getrandmax() * 5; // max price is less than 5$
-            $newShiping -> duration = '1-2 weeks';
-            $newShiping -> from_quantity = 1;
-            $newShiping -> to_quantity = 30;
-            $newShiping -> save();
-
-            // Offers
-            $newOffer = new \App\Offer;
-            $newOffer -> product_id = $newProduct -> id;
-            $newOffer -> min_quantity = 1;
-            $newOffer -> price = mt_rand() / mt_getrandmax() * 30; // max price is less than 30
-            $newOffer -> save();
-
-            // Images
-            $newImage = new \App\Image;
-            $randomImage = $this->getRandomImage();
-            $newImage -> image = $randomImage;// change image here
-            $newImage -> product_id = $newProduct -> id;
-            $newImage -> first = true;
-            $newImage -> save();
-
-            // Physical product
-            if($i % 2 == 0) {
-                $newPhysical = new \App\PhysicalProduct;
-                $newPhysical -> id = $newProduct -> id;
-                $newPhysical -> countries_option = 'all';
-                $newPhysical -> countries = '';
-                $newPhysical -> country_from = 'SRB';
-                $newPhysical -> save();
-
-            }
-            // Digital product
-            else{
-                $newDigital = new \App\DigitalProduct();
-                $newDigital -> id = $newProduct -> id;
-                $newDigital -> autodelivery = false;
-                $newDigital -> content = '';
-                $newDigital -> save();
-            }
-
-
-            $this->command->info('Created Product '.$i.'/'.$this->productsToCreate);
+        foreach ($productsData as $productData) {
+            $this->createProduct($productData);
         }
+
         $end = (microtime(true) - $start);
-
-        $this->command->info('Successfully created '.$this->productsToCreate.' products. Elapsed time: '.$this->formatTime($end));
-
+        $this->command->info('Successfully created products from JSON. Elapsed time: ' . $this->formatTime($end));
     }
 
-    /**
-     * Check if there are X (where x is specified max mount) images in array, if there are return random image, if not create new image and add it to array
-     *
-     * @return mixed|string
-     */
-    private function getRandomImage(){
+    private function readJsonFile()
+    {
+        $filePath = storage_path($this->jsonFilePath);
+        if (!file_exists($filePath)) {
+            $this->command->error('JSON file not found: ' . $filePath);
+            return [];
+        }
 
-        $faker = $this->fakerFactory;
-        //count images in array;
-        if (count($this->imagesArray) < $this->maximumAmoutOfImagesToCreate ) {
-            // if there are less than X (where x is specified max mount) images, create new image and put name it in array
-            $newImage = 'products/'. $faker->image(storage_path('app/public/products'),256,256, null, false);
-            $this->imagesArray []= $newImage;
-            return $newImage;
-        } else {
-            // if there are X (where x is specified max mount) or more images
-            $randIndex = array_rand($this->imagesArray);
-            return $this->imagesArray[$randIndex];
+        $jsonContent = file_get_contents($filePath);
+        return json_decode($jsonContent, true);
+    }
+
+    private function createProduct($data)
+    {
+        $this->command->info('Creating product: ' . $data['name']);
+
+        $category = Category::firstOrCreate(['name' => $data['category']]);
+
+        $newProduct = new Product();
+        $newProduct->name = $data['name'];
+        $newProduct->description = $data['description'];
+        $newProduct->rules = $data['rules'];
+        $newProduct->quantity = $data['quantity'];
+        $newProduct->mesure = $data['mesure'];
+        $newProduct->coins = $data['coins'];
+        $newProduct->category_id = $category->id;
+        $newProduct->user_id = \App\Vendor::inRandomOrder()->first()->id;
+        $newProduct->save();
+
+        if ($data['type'] === 'physical' && isset($data['physical'])) {
+            $this->createPhysicalProduct($newProduct->id, $data['physical']);
+        } elseif ($data['type'] === 'digital' && isset($data['digital'])) {
+            $this->createDigitalProduct($newProduct->id, $data['digital']);
+        }
+
+        if (isset($data['shipping'])) {
+            $this->createShipping($newProduct->id, $data['shipping']);
+        }
+
+        if (isset($data['offer'])) {
+            $this->createOffer($newProduct->id, $data['offer']);
         }
     }
 
-    /**
-     *  Accepts number of seconds elapsed and returns hours:minutes:seconds
-     *
-     * @param $s
-     * @return string
-     */
+    private function createPhysicalProduct($productId, $data)
+    {
+        $physicalProduct = new \App\PhysicalProduct();
+        $physicalProduct->id = $productId;
+        $physicalProduct->countries_option = $data['countries_option'];
+        $physicalProduct->countries = $data['countries'] ?? '';
+        $physicalProduct->country_from = $data['country_from'];
+        $physicalProduct->save();
+    }
+
+    private function createDigitalProduct($productId, $data)
+    {
+        $digitalProduct = new \App\DigitalProduct();
+        $digitalProduct->id = $productId;
+        $digitalProduct->autodelivery = $data['autodelivery'];
+        $digitalProduct->content = $data['content'] ?? '';
+        $digitalProduct->save();
+    }
+
+    private function createShipping($productId, $data)
+    {
+        $shipping = new \App\Shipping();
+        $shipping->product_id = $productId;
+        $shipping->name = $data['name'];
+        $shipping->price = $data['price'];
+        $shipping->duration = $data['duration'];
+        $shipping->from_quantity = $data['from_quantity'];
+        $shipping->to_quantity = $data['to_quantity'];
+        $shipping->save();
+    }
+
+    private function createOffer($productId, $data)
+    {
+        $offer = new \App\Offer();
+        $offer->product_id = $productId;
+        $offer->min_quantity = $data['min_quantity'];
+        $offer->price = $data['price'];
+        $offer->save();
+    }
+
     private function formatTime($s)
     {
         $h = floor($s / 3600);
         $s -= $h * 3600;
         $m = floor($s / 60);
         $s -= $m * 60;
-        return $h.':'.sprintf('%02d', $m).':'.sprintf('%02d', $s);
+        return $h . ':' . sprintf('%02d', $m) . ':' . sprintf('%02d', $s);
     }
 }
